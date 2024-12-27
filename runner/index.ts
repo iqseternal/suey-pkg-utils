61a9d64ccea4ef4387ddc8620fc989a47fcb6c6d
suey <yanxu@xiaoduotech.com>
2024-12-27 18:05:54 +0800

    feat: 修正加密解密RSA方式, 增加node-forge, 不再更改加密解密流程, 由使用者自行定义

--- runner/index.ts
+++ runner/index.ts
@@ -1,50 +1,22 @@
 
 
-import { apiGet, toNil, toNils, createRequest, asynced, apiPut, apiPost, apiDelete, AES, RPromiseLike } from '../src';
+import { forge } from '../src';
 
 ;(async () => {
-  {
-    const p = asynced<() => RPromiseLike<number, string>>(() => new Promise(resolve => {
-      resolve(1);
-    }))();
-    const [err, res] = await toNil(p);
-    if (err) {
-      err.reason.includes('hello');
-      return;
-    }
-    res.toFixed(1);
-  }
+  const keypair = forge.pki.rsa.generateKeyPair({
+    bits: 1024,
+    e: 0x10001
+  });
 
-  const [err, res] = await toNil(
-    asynced<() => RPromiseLike<number, string>>(() => new Promise(resolve => {
-      resolve(1);
-    }))()
-  );
+  const publicKey = keypair.publicKey;
+  const privateKey = keypair.privateKey;
 
-  if (err) {
-    err.reason.includes('hello');
-    return;
-  }
+  console.log(forge.pki.publicKeyToRSAPublicKeyPem(publicKey));
 
-  res.toFixed(1);
+  const str = publicKey.encrypt('Hello');
+  console.log(str);
 
-
-  const [p1, p2, p3] = await toNils(
-    apiGet('http://oupro.cn/api/v1.0.0/t'),
-    apiGet('http://oupro.cn/api/v1.0.0/t'),
-    apiGet('http://oupro.cn/api/v1.0.0/t')
-  );
-
-  const [p1Err, p1Res] = p1;
-  const [p2Err, p2Res] = p2;
-  const [p3Err, p3Res] = p3;
-
-  if (p1Err || p2Err || p3Err) {
-    console.log('error', p1Err?.reason.data, p2Err?.reason.data, p3Err?.reason.data);
-    return;
-  }
-
-  console.log(p1Res.data, p2Res.data, p3Res.data);
+  console.log(privateKey.decrypt(str));
 })();
 
 
--- src/encrypt/index.ts
+++ src/encrypt/index.ts
@@ -1,38 +1,124 @@
-import * as tsCrypto from 'crypto-ts';
+import * as sCryptoTs from 'crypto-ts';
 
-import * as tsMd5 from 'ts-md5';
-import type * as TsMd5Type from 'ts-md5';
+import * as sTsMd5 from 'ts-md5';
 
-import type TsRsaType from 'jsrsasign';
-import rsa from 'jsrsasign';
+import * as sJose from 'jose';
+
+import sjsr from 'jsrsasign';
+
+import sforge from 'node-forge';
 
 /**
-   * AES 加密结果
-   */
-export type CipherParams = ReturnType<typeof AES.encrypt>;
+ * ts-md5
+ *
+ * @description
+ * nodejs/浏览器
+ *
+ * @description
+ * TypeScript 的 MD5 实现
+ *
+ * 可以处理 Unicode 字符串
+ * 支持增量哈希
+ * 使用文件和 Blob
+ * 此库还包括用于以下方面的工具：
+ *
+ * 对文件或 blob 进行哈希处理
+ * 用于执行哈希的 Web Worker
+ * 一个 webworker 处理程序，用于请求对文件或 blob 进行哈希处理
+ * 基于 Promise
+ * 文件或 blob 排队等待在 WebWorker 上处理
+ *
+ * @see
+ * <p>仓库: https://www.npmjs.com/package/ts-md5</p>
+ * <p>算法: https://www.myersdaily.org/joseph/javascript/md5-text.html</p>
+ */
+export const tsMd5 = sTsMd5;
+
+/**
+ * node-forge
+ *
+ * @description
+ * nodejs/浏览器
+ *
+ * @description
+ * Forge 软件是 TLS 协议的完全原生实现 在 JavaScript 中，一组加密实用程序和一组用于 开发利用大量网络资源的 Web 应用程序。
+ *
+ * @see
+ * <p>仓库: https://www.npmjs.com/package/node-forge</p>
+ * <p>Github & Doc: https://github.com/digitalbazaar/forge/tree/main?tab=readme-ov-file#installation</p>
+ */
+export const forge = sforge;
+
+/**
+ * jsrsasign
+ *
+ * @description
+ * nodejs/浏览器
+ *
+ * @description
+ * “jsrsasign”（RSA-Sign JavaScript 库）是一个开源的免费加密库，支持纯 JavaScript 中的 RSA/RSAPSS/ECDSA/DSA 签名/验证、ASN.1、PKCS#1/5/8 私钥/公钥、X.509 证书、CRL、OCSP、CMS SignedData、TimeStamp、CAdES JSON Web 签名/令牌/密钥。
+ *
+ * @see
+ * <p>仓库: https://www.npmjs.com/package/jsrsasign</p>
+ * <p>Home: https://kjur.github.io/jsrsasign/</p>
+ * <p>文档: https://kjur.github.io/jsrsasign/api/
+ */
+export const jsr = sjsr;
+
+/**
+ * jose
+ *
+ * @description
+ * nodejs/浏览器
+ *
+ * @description
+ * jose是用于 JSON 对象签名和加密的 JavaScript 模块，支持 JSON Web 令牌 （JWT）、JSON Web 签名 （JWS）、JSON Web 加密 （JWE）、JSON Web 密钥 （JWK）、JSON Web 密钥集 （JWKS） 等。
+ * 该模块旨在跨各种 Web 互操作运行时工作，包括 Node.js、浏览器、Cloudflare Workers、Deno、Bun 等。
+ *
+ * @see
+ * <p>仓库: https://www.npmjs.com/package/jose</p>
+ * <p>文档: https://github.com/panva/jose/blob/HEAD/docs/jwt/verify/functions/jwtVerify.md
+ */
+export const jose = sJose;
+
+/**
+ * crypto-ts
+ *
+ * @description
+ * nodejs/浏览器
+ *
+ * @description
+ * 加密标准的 Typescript 库。准备好与 Angular 和其他现代 typescript 框架结合使用 AOT 和 treeshaking。
+ *
+ * @see
+ * <p>仓库: https://www.npmjs.com/package/crypto-ts</p>
+ */
+export const cryptoTs = sCryptoTs;
+
+/**
+ * AES 加密结果
+ */
+export type CipherParams = ReturnType<typeof cryptoTs.AES.encrypt>;
 
 /**
  * word 字面量
  */
-export type WordArray = Exclude<Parameters<typeof AES.encrypt>[0], string>;
+export type WordArray = Exclude<Parameters<typeof cryptoTs.AES.encrypt>[0], string>;
 
 /**
  * 加密配置
  */
-export type BufferedBlockAlgorithmConfig = Parameters<typeof AES.encrypt>[2];
-
-export const { enc, AES, SHA256, lib, algo, pad } = tsCrypto;
-export const aes = AES;
+export type BufferedBlockAlgorithmConfig = Parameters<typeof cryptoTs.AES.encrypt>[2];
 
 /**
  * AES 加密算法
  */
-export const aesEncryptAlgorithm = AES.encrypt;
+export const aesEncryptAlgorithm = cryptoTs.AES.encrypt;
 
 /**
  * AES 解密算法
  */
-export const aesDecryptAlgorithm = AES.decrypt;
+export const aesDecryptAlgorithm = cryptoTs.AES.decrypt;
 
 /**
  * 预设默认加密字面量
@@ -50,52 +136,3 @@ export const aesEncrypt = <Data extends (string | WordArray)>(data: Data, key: s
 export const aesDecrypt = <Data extends (string | CipherParams)>(data: Data, key: string | WordArray = AES_DEFAULT_KEY, config?: BufferedBlockAlgorithmConfig): WordArray => aesDecryptAlgorithm(data, key, config);
 
 
-
-export type HashingResponse = TsMd5Type.HashingResponse;
-
-export const { Md5, Md5FileHasher, ParallelHasher } = tsMd5;
-
-/**
- * md5 加密
- */
-export const md5 = Md5;
-
-export const { KEYUTIL, KJUR, RSAKey } = rsa;
-
-export type RSAPrivateKey = rsa.RSAPrivateKey;
-
-export type PublicRawRSAKeyHexResult = rsa.PublicRawRSAKeyHexResult;
-
-export interface RsaKeyOptions {
-  /**
-   * 创建 RSA key的时候, 创建多少字节的
-   *
-   * @default 512
-   */
-  bytes?: number;
-}
-
-/**
- * 初始化一个 RSA 的公钥和密钥
- * @returns [公钥, 私钥]
- */
-export const rsaGetKey = (options?: RsaKeyOptions) => {
-  const rsaKeyPair = rsa.KEYUTIL.generateKeypair('RSA', options?.bytes ?? 512);
-  const pubKey = rsa.KEYUTIL.getPEM(rsaKeyPair.pubKeyObj, 'PKCS5PRV');
-  const prvKey = rsa.KEYUTIL.getPEM(rsaKeyPair.prvKeyObj, 'PKCS8PRV');
-  return [pubKey, prvKey] as const;
-}
-
-/**
- * 使用 RSA 加密, 使用该加密之前需要先生成加密和解密密钥
- */
-export const rsaEncryptAlgorithm = (text: string, publicKey: string): string => {
-  return rsa.KJUR.crypto.Cipher.encrypt(text, rsa.KEYUTIL.getKey(publicKey) as TsRsaType.RSAKey, 'RSA') as string;
-}
-
-/**
- * 使用 RSA 解密, 使用该解密之前需要先生成加密和解密密钥
- */
-export const rsaDecryptAlgorithm = (text: string, privateKey: string): string => {
-  return rsa.KJUR.crypto.Cipher.decrypt(text, rsa.KEYUTIL.getKey(privateKey) as TsRsaType.RSAKey, 'RSA') as string;
-}
